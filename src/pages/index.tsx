05b9a70776adc1a1728bd7bce8496a6b534cde13
Quentin L <qlachet@mptronic.com>
2022-08-18 17:19:01 +0200

    Add random pokemon id fetcher

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,25 @@
+import { getOptionsForVote } from "@/utils/getRandomPokemon";
 import { trpc } from "@/utils/trpc";
+import React from "react";
 
 export default function Home() {
-  const { data, isLoading } = trpc.useQuery(["hello", { text: "Quentin"}])
-
-  if (isLoading) return <div>Loading...</div>
-
-  if(data) return <div>{data.greeting}</div>
-
+  const [hydrated, setHydrated] = React.useState(false);
+  React.useEffect(() => {
+    setHydrated(true);
+  }, []);
+  if (!hydrated) {
+    return null;
+  }
+  
+  const [first, second] = getOptionsForVote();
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokemon is Rounder ?</div>
       <div className="p-2" />
       <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
-        <div className="w-16 h-16 bg-red-200" />
+        <div className="w-16 h-16">{first}</div>
         <div className="p-8">Vs</div>
-        <div className="w-16 h-16 bg-red-200" />
+        <div className="w-16 h-16">{second}</div>
       </div>
     </div>
   );
