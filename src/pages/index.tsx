079979dab7e6a476b46a2246aad82c1b7cb8f327
Quentin L <qlachet@mptronic.com>
2022-08-19 16:26:08 +0200

    Add buttons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,11 +12,16 @@ export default function Home() {
 
   if (firstPokemon.isLoading || secondPokemon.isLoading) return null;
 
+  const voteForRoundestPokemon = (selected: number) => {
+    trpc.mutate(["vote-for-pokemon", { id: first }]);
+    updateIds(getOptionsForVote());
+  };
+
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokemon is Rounder ?</div>
       <div className="p-2" />
-      <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
+      <div className="border rounded p-14 flex justify-between max-w-2xl items-center">
         <div className="w-64 h-64 flex flex-col">
           <div className="text-xl text-center capitalize mt-[2-rem]">
             {firstPokemon.data?.name}
@@ -25,6 +30,12 @@ export default function Home() {
             src={firstPokemon.data?.sprites.front_default as any}
             className="w-full"
           />
+          <button
+            className="border rounded bg-transparent p-2 hover:bg-orange-600 hover:border-orange-600"
+            onClick={() => voteForRoundestPokemon(first)}
+          >
+            Choose
+          </button>
         </div>
         <div className="p-8">Vs</div>
         <div className="w-64 h-64 flex flex-col">
@@ -35,6 +46,12 @@ export default function Home() {
             src={secondPokemon.data?.sprites.front_default as any}
             className="w-full"
           />
+          <button
+            className="border rounded bg-transparent p-2 hover:bg-orange-600 hover:border-orange-600"
+            onClick={() => voteForRoundestPokemon(second)}
+          >
+            Choose
+          </button>
         </div>
         <div className="p-2" />
       </div>
