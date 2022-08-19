ff6dcb361f66b738c3f099a937b62f09230b5570
Quentin L <qlachet@mptronic.com>
2022-08-19 15:11:19 +0200

    Backend with pokemon data

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -1,19 +1,26 @@
-import * as trpc from '@trpc/server';
+import * as trpc from "@trpc/server";
+import { z } from "zod";
 
-import { z } from 'zod';
+import { PokemonClient } from "pokenode-ts";
 
-export const appRouter = trpc.router().query('hello', {
-        input: z
-            .object({
-                text: z.string().nullish(),
-            })
-            .nullish(),
-        resolve({ input }) {
-            return {
-                greeting: `hello ${input?.text ?? 'world'}`,
-            };
-        },
-    });
+export const appRouter = trpc
+  .router()
+  .query("get-pokemon-by-id", {
+    input: z.object({ id: z.number() }),
+    async resolve({ input }) {
+      const api = new PokemonClient();
+      const pokemon = await api.getPokemonById(input.id);
+      return pokemon;
+    },
+  })
+  .query("get-pokemon-by-name", {
+    input: z.object({ name: z.string() }),
+    async resolve({ input }) {
+      const api = new PokemonClient();
+      const pokemon = await api.getPokemonByName(input.name);
+      return pokemon;
+    },
+  });
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,42 @@
 import { getOptionsForVote } from "@/utils/getRandomPokemon";
 import { trpc } from "@/utils/trpc";
-import React from "react";
+import { useState } from "react";
 
 export default function Home() {
-  const [hydrated, setHydrated] = React.useState(false);
-  React.useEffect(() => {
-    setHydrated(true);
-  }, []);
-  if (!hydrated) {
-    return null;
-  }
-  
-  const [first, second] = getOptionsForVote();
+  const [ids, updateIds] = useState(() => getOptionsForVote());
+
+  const [first, second] = ids;
+
+  const firstPokemon = trpc.useQuery(["get-pokemon-by-id", { id: first }]);
+  const secondPokemon = trpc.useQuery(["get-pokemon-by-id", { id: second }]);
+
+  if (firstPokemon.isLoading || secondPokemon.isLoading) return null;
+
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokemon is Rounder ?</div>
       <div className="p-2" />
       <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
-        <div className="w-16 h-16">{first}</div>
+        <div className="w-64 h-64 flex flex-col">
+          <div className="text-xl text-center capitalize mt-[2-rem]">
+            {firstPokemon.data?.name}
+          </div>
+          <img
+            src={firstPokemon.data?.sprites.front_default as any}
+            className="w-full"
+          />
+        </div>
         <div className="p-8">Vs</div>
-        <div className="w-16 h-16">{second}</div>
+        <div className="w-64 h-64 flex flex-col">
+          <div className="text-xl text-center capitalize mt-[2-rem]">
+            {secondPokemon.data?.name}
+          </div>
+          <img
+            src={secondPokemon.data?.sprites.front_default as any}
+            className="w-full"
+          />
+        </div>
+        <div className="p-2" />
       </div>
     </div>
   );
