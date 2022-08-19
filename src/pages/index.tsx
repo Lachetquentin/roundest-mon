c189542c330432e15a68573477ad988eed87f7a4
Quentin L <qlachet@mptronic.com>
2022-08-19 17:22:52 +0200

    Fix button outside of div

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,9 @@ import { getOptionsForVote } from "@/utils/getRandomPokemon";
 import { trpc } from "@/utils/trpc";
 import { useState } from "react";
 
+const buttonClasses =
+  "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded";
+
 export default function Home() {
   const [ids, updateIds] = useState(() => getOptionsForVote());
 
@@ -19,41 +22,44 @@ export default function Home() {
 
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
-      <div className="text-2xl text-center">Which Pokemon is Rounder ?</div>
-      <div className="p-2" />
-      <div className="border rounded p-14 flex justify-between max-w-2xl items-center">
-        <div className="w-64 h-64 flex flex-col">
+      <div className="text-2xl text-center p-2">
+        <p>Which Pokemon is Rounder ?</p>
+      </div>
+
+      <div className="border rounded flex justify-between items-center ">
+        <div className="flex flex-col items-center p-2">
           <div className="text-xl text-center capitalize mt-[2-rem]">
             {firstPokemon.data?.name}
           </div>
           <img
             src={firstPokemon.data?.sprites.front_default as any}
-            className="w-full"
+            className="h-64 w-64"
           />
           <button
-            className="border rounded bg-transparent p-2 hover:bg-orange-600 hover:border-orange-600"
+            className={buttonClasses}
             onClick={() => voteForRoundestPokemon(first)}
           >
             Choose
           </button>
         </div>
+
         <div className="p-8">Vs</div>
-        <div className="w-64 h-64 flex flex-col">
+
+        <div className="flex flex-col items-center p-2">
           <div className="text-xl text-center capitalize mt-[2-rem]">
             {secondPokemon.data?.name}
           </div>
           <img
             src={secondPokemon.data?.sprites.front_default as any}
-            className="w-full"
+            className="h-64 w-64"
           />
           <button
-            className="border rounded bg-transparent p-2 hover:bg-orange-600 hover:border-orange-600"
+            className={buttonClasses}
             onClick={() => voteForRoundestPokemon(second)}
           >
             Choose
           </button>
         </div>
-        <div className="p-2" />
       </div>
     </div>
   );
